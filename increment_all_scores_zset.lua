d60a0e7c9dcfff235b81176a2b6d2caddea55426
Liviu Costea <email.lcostea@gmail.com>
2015-08-09 11:09:25 +0300

    using KEYS global var

--- increment_all_scores_zset.lua
+++ increment_all_scores_zset.lua
@@ -2,7 +2,7 @@
 -- I made it for http://stackoverflow.com/questions/31616886/redis-how-to-increase-all-scores-of-a-zset-with-zincrby
 
 
-local zsetMembers = redis.call('zrange', 'myzset', '0', '-1') 
-for k,member in pairs(zsetMembers) do 
-  redis.call('zincrby', 'myzset', '1', member) 
+local zsetMembers = redis.call('ZRANGE', KEYS[1], '0', '-1') 
+for _,member in pairs(zsetMembers) do 
+  redis.call('ZINCRBY', KEYS[1], '1', member) 
 end
